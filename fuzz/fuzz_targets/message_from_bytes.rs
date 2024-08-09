ff05ebfb28a8fd6c6d2bee5ebbcf248979abfc28
Matilde Morrone <contact@morrone.dev>
2024-08-09 12:02:26 +0200

    Add wire fuzzing

---
+++ fuzz/fuzz_targets/message_from_bytes.rs
@@ -0,0 +1,19 @@
+#![no_main]
+use arbitrary::Arbitrary;
+use bytes::BytesMut;
+use libfuzzer_sys::fuzz_target;
+use std::os::fd::RawFd;
+use waynest::wire::Message;
+
+#[derive(Debug, Arbitrary)]
+struct Data {
+    bytes: BytesMut,
+    fds: Vec<RawFd>,
+}
+
+fuzz_target!(|data: Data| {
+    let mut bytes = data.bytes;
+    let mut fds = data.fds;
+
+    let _ = Message::from_bytes(&mut bytes, &mut fds);
+});
---
+++ fuzz/fuzz_targets/message_to_bytes.rs
@@ -0,0 +1,11 @@
+#![no_main]
+use bytes::BytesMut;
+use libfuzzer_sys::fuzz_target;
+use waynest::wire::Message;
+
+fuzz_target!(|message: Message| {
+    let mut bytes = BytesMut::new();
+    let mut fds = Vec::new();
+
+    message.to_bytes(&mut bytes, &mut fds);
+});
