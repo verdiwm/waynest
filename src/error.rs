f4c1492787bf9743bf98a80b66b01a73a274fdf2
Matilde Morrone <contact@morrone.dev>
2024-08-09 12:34:27 +0200

    Rework code organizationa and add error type

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+use crate::wire::DecodeError;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Malformed")]
+    Malformed(#[from] DecodeError),
+    #[error("Io Error: {0}")]
+    IoError(#[from] io::Error),
+    #[error("Unknown Opcode")]
+    UnknownOpcode,
+}
+
+pub type Result<T, E = Error> = core::result::Result<T, E>;
