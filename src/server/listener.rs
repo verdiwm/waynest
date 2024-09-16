53693610f67169c085963b8bfc8ee338cd268aa6
Matilde Morrone <contact@morrone.dev>
2024-09-16 13:33:51 +0200

    Implement listener internals

--- src/server/listener.rs
+++ src/server/listener.rs
@@ -1,1 +1,20 @@
-pub struct Listener;
+use std::path::PathBuf;
+
+use pin_project_lite::pin_project;
+use rustix::fd::OwnedFd;
+use tokio::net::UnixListener;
+
+pin_project! {
+    pub struct Listener {
+        unix_listener: UnixListener,
+        _lock: OwnedFd,
+        socket_path: PathBuf,
+        lock_path:PathBuf,
+    }
+}
+
+impl Listener {
+    pub fn new() -> Self {
+        todo!()
+    }
+}
