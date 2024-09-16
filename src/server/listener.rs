b275157fabec7393309557d1e5f8163574860c49
Matilde Morrone <contact@morrone.dev>
2024-09-16 13:39:22 +0200

    Implement stream for listener

--- src/server/listener.rs
+++ src/server/listener.rs
@@ -1,20 +1,41 @@
-use std::path::PathBuf;
+use std::{
+    io,
+    path::PathBuf,
+    pin::Pin,
+    task::{Context, Poll},
+};
 
+use futures_util::Stream;
 use pin_project_lite::pin_project;
 use rustix::fd::OwnedFd;
-use tokio::net::UnixListener;
+use tokio::net::{UnixListener, UnixStream};
 
 pin_project! {
     pub struct Listener {
         unix_listener: UnixListener,
         _lock: OwnedFd,
         socket_path: PathBuf,
-        lock_path:PathBuf,
+        lock_path: PathBuf,
     }
 }
 
 impl Listener {
     pub fn new() -> Self {
         todo!()
+    }
+}
+
+impl Stream for Listener {
+    type Item = io::Result<UnixStream>;
+
+    fn poll_next(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<io::Result<UnixStream>>> {
+        match self.unix_listener.poll_accept(cx) {
+            Poll::Ready(Ok((stream, _))) => Poll::Ready(Some(Ok(stream))),
+            Poll::Ready(Err(err)) => Poll::Ready(Some(Err(err))),
+            Poll::Pending => Poll::Pending,
+        }
     }
 }
