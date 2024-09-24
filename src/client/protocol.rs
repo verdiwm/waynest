8d67a666f0fe0c2ed00ff94f1029e831d96146c5
Federico Maria Morrone <contact@morrone.dev>
2024-09-24 14:41:47 +0200

    Generate unstable docs features

--- src/client/protocol.rs
+++ src/client/protocol.rs
@@ -1,17 +1,25 @@
 pub mod core;
 #[cfg(feature = "cosmic")]
+#[cfg_attr(docsrs, doc(cfg(feature = "cosmic")))]
 pub mod cosmic;
 #[cfg(feature = "frog")]
+#[cfg_attr(docsrs, doc(cfg(feature = "frog")))]
 pub mod frog;
 #[cfg(feature = "plasma")]
+#[cfg_attr(docsrs, doc(cfg(feature = "plasma")))]
 pub mod plasma;
 #[cfg(feature = "stable")]
+#[cfg_attr(docsrs, doc(cfg(feature = "stable")))]
 pub mod stable;
 #[cfg(feature = "staging")]
+#[cfg_attr(docsrs, doc(cfg(feature = "staging")))]
 pub mod staging;
 #[cfg(feature = "unstable")]
+#[cfg_attr(docsrs, doc(cfg(feature = "unstable")))]
 pub mod unstable;
 #[cfg(feature = "weston")]
+#[cfg_attr(docsrs, doc(cfg(feature = "weston")))]
 pub mod weston;
 #[cfg(feature = "wlr")]
+#[cfg_attr(docsrs, doc(cfg(feature = "wlr")))]
 pub mod wlr;
