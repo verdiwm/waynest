0bddb794a12dad3a4b32891956b36abf389b3509
Matilde Morrone <contact@morrone.dev>
2024-09-26 22:00:16 +0200

    Add macros to generate protocols

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -22,3 +22,21 @@ pub fn derive_dispatcher(input: TokenStream) -> TokenStream {
     }
     .into()
 }
+
+#[proc_macro]
+#[cfg(feature = "gen")]
+pub fn server_protocol(input: TokenStream) -> TokenStream {
+    use syn::LitStr;
+
+    let _xml = parse_macro_input!(input as LitStr);
+    quote! {}.into()
+}
+
+#[proc_macro]
+#[cfg(feature = "gen")]
+pub fn client_protocol(input: TokenStream) -> TokenStream {
+    use syn::LitStr;
+
+    let _xml = parse_macro_input!(input as LitStr);
+    quote! {}.into()
+}
