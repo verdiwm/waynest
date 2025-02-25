27c209038d012002d055259a30e827dbe60221d2
Nova <technobaboo@gmail.com>
2025-02-24 19:30:43 -0800

    feat(macros): destructor body impl

--- src/client/protocol.rs
+++ src/client/protocol.rs
@@ -1,31 +1,30 @@
-pub mod core;
-#[cfg(feature = "cosmic")]
-#[cfg_attr(docsrs, doc(cfg(feature = "cosmic")))]
-pub mod cosmic;
-#[cfg(feature = "frog")]
-#[cfg_attr(docsrs, doc(cfg(feature = "frog")))]
-pub mod frog;
-#[cfg(feature = "hyprland")]
-#[cfg_attr(docsrs, doc(cfg(feature = "hyprland")))]
-pub mod hyprland;
-#[cfg(feature = "ivi")]
-#[cfg_attr(docsrs, doc(cfg(feature = "ivi")))]
-pub mod ivi;
-#[cfg(feature = "plasma")]
-#[cfg_attr(docsrs, doc(cfg(feature = "plasma")))]
-pub mod plasma;
-#[cfg(feature = "stable")]
-#[cfg_attr(docsrs, doc(cfg(feature = "stable")))]
-pub mod stable;
-#[cfg(feature = "staging")]
-#[cfg_attr(docsrs, doc(cfg(feature = "staging")))]
-pub mod staging;
-#[cfg(feature = "unstable")]
-#[cfg_attr(docsrs, doc(cfg(feature = "unstable")))]
-pub mod unstable;
+pub mod core;#[cfg(feature = "wlr")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "wlr")))]
+        pub mod wlr;
 #[cfg(feature = "weston")]
-#[cfg_attr(docsrs, doc(cfg(feature = "weston")))]
-pub mod weston;
-#[cfg(feature = "wlr")]
-#[cfg_attr(docsrs, doc(cfg(feature = "wlr")))]
-pub mod wlr;
+        #[cfg_attr(docsrs, doc(cfg(feature = "weston")))]
+        pub mod weston;
+#[cfg(feature = "cosmic")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "cosmic")))]
+        pub mod cosmic;
+#[cfg(feature = "unstable")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "unstable")))]
+        pub mod unstable;
+#[cfg(feature = "stable")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "stable")))]
+        pub mod stable;
+#[cfg(feature = "plasma")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "plasma")))]
+        pub mod plasma;
+#[cfg(feature = "ivi")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "ivi")))]
+        pub mod ivi;
+#[cfg(feature = "frog")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "frog")))]
+        pub mod frog;
+#[cfg(feature = "staging")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "staging")))]
+        pub mod staging;
+#[cfg(feature = "hyprland")]
+        #[cfg_attr(docsrs, doc(cfg(feature = "hyprland")))]
+        pub mod hyprland;
